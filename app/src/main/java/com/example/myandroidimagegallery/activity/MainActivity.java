b5859fcaff30b842b8cc8ff37e7fb021fbaa004d
DarushDev <darushfaqiri@gmail.com>
2017-07-17 18:42:47 +0800

    Add AppController to control network request queue

--- app/src/main/java/com/example/myandroidimagegallery/activity/MainActivity.java
+++ app/src/main/java/com/example/myandroidimagegallery/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.myandroidimagegallery;
+package com.example.myandroidimagegallery.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.myandroidimagegallery.R;
 
 public class MainActivity extends AppCompatActivity {
 
